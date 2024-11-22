a7fdaf78bdd22d0260c1f4606f0294692d836e69
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2024-11-22 13:36:38 -0500

    Filled out Array's methods

--- Array.java
+++ Array.java
@@ -1,3 +1,45 @@
-public class Array<T> {
+import java.util.Stack;
 
+public class Array<T> {
+    private int length;
+    private Stack<T> stack;
+
+    public Array<T>() {
+        stack = new Stack<>();
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean at(int index, T item) {
+        if (index >= 0 && index <= size) {
+            Stack<T> temp = new Stack<>();
+            for (int i = 1; i <= length - index; ++i) {
+                temp.push(stack.pop());
+            }
+            stack.push(item);
+            size++;
+            for (int i = 1; i <= length - index; ++i) {
+                stack.push(temp.pop());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public T at(int index) {
+        if (index >= 0 && index < size) {
+            Stack<T> temp = new Stack<>();
+            for (int i = 1; i <= size - index; ++i) {
+                temp.push(stack.pop());
+            }
+            T item = stack.peek();
+            for (int i = 1; i <= size - index; ++i) {
+                stack.push(temp.pop());
+            }
+            return item;
+        }
+        return null;
+    }
 }
